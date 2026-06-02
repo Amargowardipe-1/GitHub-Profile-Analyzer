249276083543aec4cc5f9b41ab497cf19bc3fe58
amar gowardipe <amargowar@gmail.com>
2026-06-02 09:47:35 +0530

    ba

--- server/app.js
+++ server/app.js
@@ -13,7 +13,8 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://git-hub-profile-analyzer-z3mk-c5s9g48hb.vercel.app"
+    "https://git-hub-profile-analyzer-z3mk-c5s9g48hb.vercel.app",
+    "https://git-hub-profile-analyzer-z3mk.vercel.app/"
   
   ],
   credentials: true
