6aeb0e9a5c6e852749cf6ce0e0dc38782949517f
amar gowardipe <amargowar@gmail.com>
2026-06-02 10:14:57 +0530

    u

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://github-profile-analyzer-db4b.onrender.com",
 });
 
 export default api;
